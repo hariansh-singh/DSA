0c96456215d7a815e3e2d30d41189280c5686d73
hariansh-singh <harianshsingh13@example.com>
2024-03-13 01:52:19 +0530

    Print Array Extreme Points Problem solved.

---
+++ Array/ExtremePoint.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[8] = {2,5,13,7,9,4,1,6};
+    int size = 8;
+    
+    int start = 0;
+    int end = size - 1;
+    
+    while(true) {
+        if(start>end) {
+            break;
+        }
+        cout << arr[start] << " ";
+        cout << arr[end] << " ";
+        
+        start++;
+        end--;
+        
+    }
+
+    return 0;
+}
