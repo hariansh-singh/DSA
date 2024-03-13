2548b23619ce0af281c296f6d3099c89507bcf5c
hariansh-singh <harianshsingh13@example.com>
2024-03-13 16:42:30 +0530

    Print Array Extreme Points solution modified

--- Array/ExtremePoint.cpp
+++ Array/ExtremePoint.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main()
 {
-    int arr[8] = {2,5,13,7,9,4,1,6};
-    int size = 8;
+    int arr[9] = {2,5,13,7,9,4,1,6,32};
+    int size = 9;
     
     int start = 0;
     int end = size - 1;
     
-    while(true) {
-        if(start>end) {
+    while(start<=end) {
+        if (start == end) {
+            cout << arr[start] << " ";
             break;
         }
         cout << arr[start] << " ";
@@ -22,3 +23,4 @@ int main()
 
     return 0;
 }
+ 
