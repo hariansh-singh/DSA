26f20cfeb9a9895c57429127de0baea7958d644f
hariansh-singh <harianshsingh13@gmail.com>
2024-03-19 19:29:54 +0530

    Fixed bugs in the code.

--- Array/BinarySearch.cpp
+++ Array/BinarySearch.cpp
@@ -7,7 +7,7 @@ int BinarySearch(vector <int> arr, int n) {
 
     int start = 0;
     int end = arr.size() - 1;
-    int mid = (start + end) / 2;
+    int mid = start + (end - start) / 2;
     
     while(start <= end) {
 
@@ -27,7 +27,7 @@ int BinarySearch(vector <int> arr, int n) {
             start = mid + 1;
         }
 
-        mid = (start + end) / 2;
+        mid = start + (end - start) / 2;
     }
     return -1;
 }
