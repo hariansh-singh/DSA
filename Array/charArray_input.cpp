7b083f216923eac90d9d86f4e8c8da381d4ef2b0
hariansh-singh <harianshsingh13@gmail.com>
2024-04-03 13:41:47 +0530

    started string concept

---
+++ Array/charArray_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char ch[10];
+    cout << "Enter Your Name: "; 
+    cin >> ch;
+    cout << endl;
+    cout << "Your Name is " << ch << endl;
+    
+    return 0;
+}
---
+++ Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+
+    cout << endl;
+    cout << "Your Name is " << ch << " hai" << endl;
+    
+    return 0;
+}
