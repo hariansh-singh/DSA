fa688d0eac8893fbc83811e0640452981d020df2
hariansh-singh <harianshsingh13@gmail.com>
2024-04-03 13:51:55 +0530

    Updated Code

--- Array/charArray_input.cpp
+++ Array/charArray_input.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <strings.h>
 using namespace std;
 
 int main() {
 
     char ch[10];
     cout << "Enter Your Name: "; 
-    cin >> ch;
+    cin.getline(ch, 10);
     cout << endl;
     cout << "Your Name is " << ch << endl;
     
