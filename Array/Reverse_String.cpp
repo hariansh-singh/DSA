482aa1554ebe59781f8a4a519a047010a9ebb7b3
hariansh-singh <harianshsingh13@gmail.com>
2024-04-03 22:00:12 +0530

    Add code to reverse a string

---
+++ Array/Reverse_String.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void ReverseString(char arr[6]) {
+    int i = 0;
+    int j = strlen(arr) - 1;
+
+    while (i < j) {
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+
+    for (int k = 0; k < strlen(arr); k++) { 
+        cout << arr[k] << " ";
+    }
+}
+
+int main() {
+    
+    char arr[6] = {'h','a','r','r','y'};
+    for (int k = 0; k < strlen(arr); k++) { 
+        cout << arr[k] << " ";
+    }
+    cout << endl;
+
+    ReverseString(arr);
+
+    return 0;
+}
